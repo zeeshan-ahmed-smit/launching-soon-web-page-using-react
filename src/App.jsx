2301e4733850756b4e18a16005add1d04c53fa5a
Zeeshan <ka265923@gmail.com>
2023-12-08 23:20:09 +0500

    launching soon

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import BgImg from './assets/bgImg.gif'
+import './styles.css'
+
+const App = () => {
+  return (
+    <div className='page'>
+      <div className='overlay'></div>
+      <img className='bg-img' src={BgImg}/>
+      <div className="page-content">
+        <h1>Launching Soon</h1>
+        <h3>Leave your email and we'll let you know once the site goes live.</h3>
+      </div>
+      <div className="clock">
+        <FlipClockCountdown className='flip-clock'
+          to={new Date().getTime() + 24 * 3600 * 1000 + 5000}
+          labels={['DAYS', 'HOURS', 'MINUTES', 'SECONDS']}
+          duration={0.5}
+        >
+          Finished
+        </FlipClockCountdown>
+      </div>
+    </div>
+  );
+};
+
+export default App;
